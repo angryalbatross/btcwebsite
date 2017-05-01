ce3349990733ecbba9131d242f2309c8aee1821f
Dan B <daniel.burgett@gmail.com>
2017-04-30 19:44:49 -0500

    Committing fixes to form validation

--- themes/hugo-agency-theme/static/js/agency.js
+++ themes/hugo-agency-theme/static/js/agency.js
@@ -46,22 +46,42 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-// Async contact form
-$('form[id=contactForm]').submit(function(){
-  $.post($(this).attr('action'), $(this).serialize(), function(res){
-    $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    if (res.code == "200")
-      $('form[id=contactForm] #success').show();
-    }).fail(function(){
-    $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    $('form[id=contactForm] #error').show();
-  });
-  return false;
-});
+// // Async contact form
+// $('form[id=contactForm]').submit(function(){
+//   debugger;
+//   $.post($(this).attr('action'), $(this).serialize(), function(res){
+//     $('form[id=contactForm] #success').hide();
+//     $('form[id=contactForm] #error').hide();
+//     if (res.code == "200")
+//       $('form[id=contactForm] #success').show();
+//     }).fail(function(){
+//     $('form[id=contactForm] #success').hide();
+//     $('form[id=contactForm] #error').hide();
+//     $('form[id=contactForm] #error').show();
+//   });
+//   return false;
+// });
+//
+// // Contact form validation
+// $.validate({
+//   modules : 'html5, toggleDisabled'
+// });
 
-// Contact form validation
-$.validate({
-  modules : 'html5, toggleDisabled'
-});
+// Ajax contact
+function contactForm () {
+  var form = $('#contactForm')
+  form.submit(function () {
+    $this = $(this)
+    $.post($(this).attr('action'),
+      $this.serialize(),
+      function () {
+        $this[0].reset() // clear form
+
+        $('#contact-message')
+        .html('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>Thank you for getting in touch. We will get back to you soon!</div>')
+        .fadeIn()
+      }
+      , 'json')
+    return false
+  })
+}
